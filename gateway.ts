eae3e12b6bbaaccff4213e60f6490a794355bd37
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2021-06-15 15:00:18 +0700

    feat(cluster): add cluster worker

--- gateway.ts
+++ gateway.ts
@@ -1,34 +1,64 @@
-import express from "express";
+import * as express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { ApolloGateway } from "@apollo/gateway";
+var cluster = require("cluster");
 
-async function start() {
-  try {
-    const gateway = new ApolloGateway({
+class ServerInstance {
+  private static instance: ServerInstance = null;
+  private gateway: ApolloGateway;
+  private apolloServer: ApolloServer;
+  private app = express();
+
+  public static getInstance(): ServerInstance {
+    if (!ServerInstance.instance) {
+      const instance = new ServerInstance();
+      ServerInstance.instance = instance;
+    }
+    return ServerInstance.instance;
+  }
+
+  private reInit() {
+    this.gateway = new ApolloGateway({
+      experimental_pollInterval: 1000,
       serviceList: [
         {
           name: "cat",
-          url: "http://localhost:5001",
+          url: "http://localhost:5008",
         },
       ],
     });
-
-    const apolloServer = new ApolloServer({
-      gateway,
+    this.apolloServer = new ApolloServer({
+      gateway: this.gateway,
       introspection: true,
       playground: true,
       subscriptions: false,
     });
+  }
 
-    await apolloServer.start();
-    const app = express();
-    apolloServer.applyMiddleware({ app });
-
-    app.listen(5000);
+  private async listen() {
+    await this.apolloServer.start();
+    this.apolloServer.applyMiddleware({ app: this.app });
+    this.app.listen(5000);
     console.log(process.memoryUsage());
-  } catch (e) {
-    await start();
+  }
+
+  public async start() {
+    this.reInit();
+    await this.listen();
   }
 }
 
-start();
+async function main() {
+  if (cluster.isMaster) {
+    cluster.fork();
+    cluster.on("exit", function (worker, code, signal) {
+      cluster.fork();
+    });
+  }
+
+  if (cluster.isWorker) {
+    await ServerInstance.getInstance().start();
+  }
+}
+
+main();
